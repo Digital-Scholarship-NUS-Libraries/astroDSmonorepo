c81492836ced07806a716e8ee20952f3eeee43e7
gtnbssn <gaetan.boisson@gmail.com>
2022-07-05 18:19:06 +0800

    new ImageFloat component

--- packages/ui/index.js
+++ packages/ui/index.js
@@ -1,4 +1,5 @@
 export { default as Header } from './components/Header.astro'
 export { default as NavBar } from './components/NavBar.astro'
 export { default as Footer } from './components/Footer.astro'
+export { default as ImageFloat } from './components/ImageFloat.astro'
 export { default as BaseLayout } from './layouts/BaseLayout.astro'
