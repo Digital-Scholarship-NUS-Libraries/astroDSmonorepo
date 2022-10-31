72d5c628709d31a65f0231ce885661d575e17366
gtnbssn <gaetan.boisson@gmail.com>
2022-10-31 14:20:52 +0800

    wayang archive: search bar

--- sites/wayangKulitImageDatabase/src/components/SearchBar.svelte
+++ sites/wayangKulitImageDatabase/src/components/SearchBar.svelte
@@ -15,14 +15,32 @@
   });
 
   let searchTerm = "";
+
   let filteredItems = [];
   $: {
     if (searchTerm) {
       filteredItems = $aliasesAtom.filter((item) =>
-        item.alias.toLowerCase().includes(searchTerm.toLowerCase())
+        item.alias.toLowerCase().includes(searchTerm.toLowerCase().trim())
       );
     } else {
       filteredItems = $aliasesAtom;
+    }
+  }
+
+  const onKeyDown = (e) => {
+    switch (e.code) {
+      case "Escape":
+        searchTerm = "";
+        break;
+      case "ArrowUp":
+        console.log("up");
+        break;
+      case "ArrowDown":
+        console.log("down");
+        break;
+      case "Enter":
+        console.log("Enter");
+        break;
     }
   }
 </script>
@@ -36,12 +54,19 @@
     placeholder="filter items"
     disabled={$aliasesAtom.length ? false : true}
     bind:value={searchTerm}
+    on:keydown={onKeyDown}
   />
 
   {#if searchTerm.length}
-    <ul class="absolute px-2 w-full rounded-md border-gray-200 bg-neutral-50 list-none max-h-60 overflow-y-scroll">
+    <ul
+      class="absolute px-2 w-full rounded-md border-gray-200 bg-neutral-50 list-none max-h-60 overflow-y-scroll"
+    >
       {#each filteredItems as item}
-        <li class="px-2 rounded-md hover:bg-neutral-200">{item.alias}</li>
+        <a
+          href={"https://blooming-badlands-24637.herokuapp.com/alias/?alias=" +
+            item.alias}
+          ><li class="px-2 rounded-md hover:bg-neutral-200">{item.alias}</li></a
+        >
       {/each}
     </ul>
   {/if}
