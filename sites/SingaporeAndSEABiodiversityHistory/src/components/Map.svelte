4ed99303b4130eaf29b143b69f159fd736341d74
tohlh <tohliheng@gmail.com>
2023-05-09 23:05:04 +0800

    add map markers with filtering

--- sites/SingaporeAndSEABiodiversityHistory/src/components/Map.svelte
+++ sites/SingaporeAndSEABiodiversityHistory/src/components/Map.svelte
@@ -1,56 +1,43 @@
-<script lang="ts">
+<script>
   // OpenLayers
   import "ol/ol.css";
   import Map from "ol/Map";
   import TileLayer from "ol/layer/Tile";
+  import { OSM } from "ol/source";
   import { fromLonLat } from "ol/proj";
 
-  import { Feature } from "ol";
   import { Point } from "ol/geom";
-  import * as Source from "ol/source";
-  import * as Layer from "ol/layer";
+  import { Feature } from "ol";
   import { Circle, Fill, Style } from "ol/style";
+  import * as Layer from "ol/layer";
+  import * as Source from "ol/source";
 
   // Svelte
+  import { setContext } from "svelte";
   import View from "ol/View";
-  import OSM from "ol/source/OSM";
 
-  import { csv } from "d3";
+  import { filteredAnimals } from "../store/animals.js";
 
   // Exports
+  export let lat;
+  export let lon;
+  export let zoom;
+
   let mapId = "olmap";
   // Local state
   let map = null;
 
-  // functions
-  let features : Point = [];
   const setupMap = (node) => {
     const osmLayer = new TileLayer({
       source: new OSM(),
     });
 
-    csv("/data/SG_Animal_Naturalised.csv").then((animals) => {
-      animals.forEach((animal) => {
-        // console.log(animal);
-        const longitude = animal["Longitude (First published)"];
-        const latitude = animal["Latitude (First published)"];
-        features = [
-          ...features,
-          new Feature({
-            geometry: new Point(fromLonLat([longitude, latitude])),
-          }),
-        ];
-      });
-    });
-
-    const vectorSource = new Source.Vector({
-      features,
-    });
-    const vectorLayer = new Layer.Vector({
-      source: vectorSource,
+    const markerSource = new Source.Vector();
+    const markerLayer = new Layer.Vector({
+      source: markerSource,
       style: new Style({
         image: new Circle({
-          radius: 3,
+          radius: 5,
           fill: new Fill({ color: "red" }),
         }),
       }),
@@ -58,11 +45,32 @@
 
     map = new Map({
       target: node.id,
-      layers: [osmLayer, vectorLayer],
+      layers: [osmLayer, markerLayer],
       view: new View({
-        center: fromLonLat([103.8, 1.35]),
-        zoom: 10,
+        center: fromLonLat([lon, lat]),
+        zoom: zoom,
       }),
+    });
+
+    filteredAnimals.subscribe((animals) => {
+      markerSource.clear();
+      animals.forEach((animal) => {
+        if (
+          animal["Longitude (First published)"] &&
+          animal["Latitude (First published)"]
+        ) {
+          markerSource.addFeature(
+            new Feature({
+              geometry: new Point(
+                fromLonLat([
+                  animal["Longitude (First published)"],
+                  animal["Latitude (First published)"],
+                ])
+              ),
+            })
+          );
+        }
+      });
     });
 
     return {
@@ -77,7 +85,11 @@
   };
 </script>
 
-<div id={mapId} class="map" use:setupMap />
+<div id={mapId} class="map" use:setupMap>
+  {#if map}
+    <slot />
+  {/if}
+</div>
 
 <style>
   .map {
