920b092765fefad7b75e881d52f9c9ae49bbb0bb
gtnbssn <gaetan.boisson@gmail.com>
2023-06-14 18:00:40 +0800

    switch map to maplibre

--- sites/SingaporeAndSEABiodiversityHistory/src/components/MapView.svelte
+++ sites/SingaporeAndSEABiodiversityHistory/src/components/MapView.svelte
@@ -1,13 +1,117 @@
 <script lang="ts">
-  import Map from "./Map.svelte";
+  // import Map from "./Map.svelte";
   import AnimalFilterDrawer from "./AnimalFilterDrawer.svelte";
+  import { filteredAnimalsGeoJSON } from "../store/animals";
+  import {
+    MapLibre,
+    GeoJSON,
+    CircleLayer,
+    SymbolLayer,
+    Popup,
+    hoverStateFilter,
+  } from "svelte-maplibre";
+
+  // filteredAnimalsGeoJSON.subscribe((v) => console.log(v));
+  $: console.log($filteredAnimalsGeoJSON);
+  let clickedFeature: Record<string, any> | null = null;
 </script>
 
 <div class="flex h-screen">
-  <div class="w-8/12">
-    <Map lon={103.8} lat={1.35} zoom={10} />
-  </div>
+  <!-- <div class="w-8/12"> -->
+  <!--   <Map lon={103.8} lat={1.35} zoom={10} /> -->
+  <!-- </div> -->
+  <MapLibre
+    style="https://basemaps.cartocdn.com/gl/positron-gl-style/style.json"
+    class="w-8/12"
+    center={[103.8, 1.35]}
+    zoom={11}
+    maxBounds={[
+      [103.4, 1.0],
+      [104.3, 1.7],
+    ]}
+    standardControls
+  >
+    <GeoJSON
+      id="animals"
+      data={$filteredAnimalsGeoJSON}
+      cluster={{ radius: 500, maxZoom: 16 }}
+      generateId
+    >
+      <CircleLayer
+        applyToClusters
+        hoverCursor="pointer"
+        paint={{
+          // Use step expressions (https://maplibre.org/maplibre-gl-js-docs/style-spec/#expressions-step)
+          // with three steps to implement three types of circles:
+          //   * Blue, 20px circles when point count is less than 100
+          //   * Yellow, 30px circles when point count is between 100 and 750
+          //   * Pink, 40px circles when point count is greater than or equal to 750
+          "circle-color": [
+            "step",
+            ["get", "point_count"],
+            "#51bbd6",
+            100,
+            "#f1f075",
+            750,
+            "#f28cb1",
+          ],
+          "circle-radius": [
+            "step",
+            ["get", "point_count"],
+            20,
+            100,
+            30,
+            750,
+            40,
+          ],
+          "circle-stroke-color": "#f00",
+          "circle-stroke-width": 1,
+          "circle-stroke-opacity": hoverStateFilter(0, 1),
+        }}
+      />
+      <SymbolLayer
+        applyToClusters
+        layout={{
+          "text-field": ["format", ["get", "point_count_abbreviated"]],
+          "text-size": 12,
+          "text-offset": [0, -0.1],
+        }}
+      />
+      <CircleLayer
+        applyToClusters={false}
+        hoverCursor="pointer"
+        paint={{
+          "circle-color": "#11b4da",
+          // "circle-color": ["<"],
+          "circle-radius": 4,
+          "circle-stroke-width": 1,
+          "circle-stroke-color": "#f00",
+          "circle-stroke-opacity": hoverStateFilter(0, 1),
+        }}
+        on:click={(e) => (clickedFeature = e.detail.features?.[0]?.properties)}
+      >
+        <Popup openOn={"click"} closeOnClickInside let:features>
+          {@const props = features?.[0]?.properties}
+          <ul>
+            {#each Object.entries(props) as [key, value]}
+              {#if value}
+                <li><strong>{key}:</strong> {value}</li>
+              {/if}
+            {/each}
+          </ul>
+        </Popup>
+      </CircleLayer>
+    </GeoJSON>
+  </MapLibre>
   <div class="w-4/12">
     <AnimalFilterDrawer />
   </div>
 </div>
+
+<style>
+  :global(.maplibregl-popup-content) {
+    max-height: 300px;
+    max-width: 250px;
+    overflow: scroll;
+  }
+</style>
--- sites/SingaporeAndSEABiodiversityHistory/src/store/animals.js
+++ sites/SingaporeAndSEABiodiversityHistory/src/store/animals.js
@@ -29,6 +29,94 @@ export const filterAttributes = writable({
   activeRemoval: false,
 })
 
+export const filteredAnimalsGeoJSON = derived(
+  [animals, filterAttributes],
+  ([$animals, $filterAttributes], set) => {
+    const animalsFeatures = $animals
+      .filter(animal => (
+        (Number(animal["Date: yyyy (First published)"]) >= $filterAttributes.minYear) &&
+        Number(animal["Date: yyyy (First published)"]) <= $filterAttributes.maxYear
+      ))
+      .filter(animal => (
+        (animal["Fauna"] === "Aves" && $filterAttributes.fauna.aves) ||
+        (animal["Fauna"] === "Freshwater" && $filterAttributes.fauna.freshwater) ||
+        (animal["Fauna"] === "Marine" && $filterAttributes.fauna.marine) ||
+        (animal["Fauna"] === "Terrestrial" && $filterAttributes.fauna.terrestrial)
+      ))
+      .filter(animal => (
+        (animal["Introduction_all"].split(" | ").includes("Agricultural trade") && $filterAttributes.introPathway.agricultural) ||
+        (animal["Introduction_all"].split(" | ").includes("Biological control") && $filterAttributes.introPathway.biological) ||
+        (animal["Introduction_all"].split(" | ").includes("Consumption trade") && $filterAttributes.introPathway.consumption) ||
+        (animal["Introduction_all"].split(" | ").includes("Corridors") && $filterAttributes.introPathway.corridor) ||
+        (animal["Introduction_all"].split(" | ").includes("Fishing") && $filterAttributes.introPathway.fishing) ||
+        (animal["Introduction_all"].split(" | ").includes("Horticultural trade") && $filterAttributes.introPathway.horticultural) ||
+        (animal["Introduction_all"].split(" | ").includes("Mercy release") && $filterAttributes.introPathway.mercyRelease) ||
+        (animal["Introduction_all"].split(" | ").includes("Pet trade") && $filterAttributes.introPathway.petTrade) ||
+        (animal["Introduction_all"].split(" | ").includes("Transport") && $filterAttributes.introPathway.transport)
+      ))
+      .filter(animal => (
+        animal["Extinction (Y/N)"] === ($filterAttributes.extinction ? "Y" : "N")
+      ))
+      .filter(animal => (
+        animal["Active removal (Y/N)"] === ($filterAttributes.activeRemoval ? "Y" : "N")
+      ))
+      .filter(animal => (Number(animal["Longitude (First published)"]) && Number(animal["Latitude (First published)"])))
+      .map(animal => {
+        return {
+          "type": "Feature",
+          "geometry": { "type": "Point", "coordinates": [Number(animal["Longitude (First published)"]), Number(animal["Latitude (First published)"])] },
+          "properties": {
+            "Status": animal["Status"],
+            "Species name (Current accepted)": animal["Species name (Current accepted)"],
+            "Authority, Year": animal["Authority, Year"],
+            "Common Name": animal["Common Name"],
+            "Species  (Previous names)": animal["Species  (Previous names)"],
+            "Fauna": animal["Fauna"],
+            "Kingdom": animal["Kingdom"],
+            "Phylum": animal["Phylum"],
+            "Class": animal["Class"],
+            "Order": animal["Order"],
+            "Family": animal["Family"],
+            "Genus": animal["Genus"],
+            "Species": animal["Species"],
+            "Native_biogeog": animal["Native_biogeog"],
+            "Native_geog": animal["Native_geog"],
+            "Species name (First published)": animal["Species name (First published)"],
+            "Reference (First published)": animal["Reference (First published)"],
+            "Specific Page No. (First published)": animal["Specific Page No. (First published)"],
+            "Date: yyyy (First published)": animal["Date: yyyy (First published)"],
+            "Latitude (First published)": animal["Latitude (First published)"],
+            "Longitude (First published)": animal["Longitude (First published)"],
+            "Location (First published)": animal["Location (First published)"],
+            "Species name (First sighting)": animal["Species name (First sighting)"],
+            "Reference (First sighting)": animal["Reference (First sighting)"],
+            "Date: dd/mm/yyyy (First sighting)": animal["Date: dd/mm/yyyy (First sighting)"],
+            "Year (First sighting)": animal["Year (First sighting)"],
+            "Latitude (First sighting)": animal["Latitude (First sighting)"],
+            "Longitude (First sighting)": animal["Longitude (First sighting)"],
+            "Location (First sighting)": animal["Location (First sighting)"],
+            "Introduction pathway": animal["Introduction pathway"],
+            "Introduction_mode": animal["Introduction_mode"],
+            "Introduction_all": animal["Introduction_all"],
+            "Introduction pathway Reference": animal["Introduction pathway Reference"],
+            "Extinction (Y/N)": animal["Extinction (Y/N)"],
+            "Year (Extinction)": animal["Year (Extinction)"],
+            "Extinction Reference": animal["Extinction Reference"],
+            "Active removal (Y/N)": animal["Active removal (Y/N)"],
+            "Active removal Reference": animal["Active removal Reference"],
+            "References: Subsequent records (Surname, journal short form, vol(issue) (year) page)": animal["References: Subsequent records (Surname, journal short form, vol(issue) (year) page)"],
+            "Additional Remarks": animal["Additional Remarks"]
+          }
+        };
+      });
+    const animalsGeoJSON = {
+      "type": "FeatureCollection",
+      "features": animalsFeatures
+    };
+    set(animalsGeoJSON);
+  }
+)
+
 export const filteredAnimals = derived(
   [animals, filterAttributes],
   ([$animals, $filterAttributes], set) => {
