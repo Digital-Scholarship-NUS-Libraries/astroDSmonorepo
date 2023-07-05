aa3e9f5a1926a997108263bb6b69583d9672dba7
gtnbssn <gaetan.boisson@gmail.com>
2023-07-05 19:03:40 +0800

    working TMS layer from libmaps, broken COG experiment

---
+++ sites/SingaporeAndSEABiodiversityHistory/src/components/COGLayer.svelte
@@ -0,0 +1,79 @@
+<script lang="ts">
+  import { Layer } from "svelte-maplibre";
+
+  import maplibregl, { Map, RasterSourceSpecification } from "maplibre-gl";
+  import { Pool, fromUrl } from "geotiff";
+  import { encode } from "fast-png";
+  import { onMount } from "svelte";
+
+  export let cogURL;
+
+  /**
+   * transform x/y/z to webmercator-bbox
+   * @param x
+   * @param y
+   * @param z
+   * @returns {number[]} [minx, miny, maxx, maxy]
+   */
+  const merc = (x: number, y: number, z: number): number[] => {
+    const GEO_R = 6378137;
+    const orgX = -1 * ((2 * GEO_R * Math.PI) / 2);
+    const orgY = (2 * GEO_R * Math.PI) / 2;
+    const unit = (2 * GEO_R * Math.PI) / Math.pow(2, z);
+    const minx = orgX + x * unit;
+    const maxx = orgX + (x + 1) * unit;
+    const miny = orgY - (y + 1) * unit;
+    const maxy = orgY - y * unit;
+    return [minx, miny, maxx, maxy];
+  };
+
+  const generateCogSource = async (
+    url: string
+  ): Promise<{ source: RasterSourceSpecification }> => {
+    const tiff = await fromUrl(url);
+    const pool = new Pool();
+    maplibregl.addProtocol("cog", (params, callback) => {
+      const segments = params.url.split("/");
+      const [z, x, y] = segments.slice(segments.length - 3).map(Number);
+      const bbox = merc(x, y, z);
+      const size = 256;
+      tiff
+        .readRasters({
+          bbox,
+          samples: [0, 1, 2, 3],
+          width: size,
+          height: size,
+          interleave: true,
+          pool,
+        })
+        .then((data) => {
+          const img = new ImageData(
+            //@ts-ignore
+            new Uint8ClampedArray(data),
+            size,
+            size
+          );
+          const png = encode(img);
+          callback(null, png, null, null);
+        });
+      return { cancel: () => {} };
+    });
+    const source: RasterSourceSpecification = {
+      type: "raster",
+      tiles: [`cog://${url.split("://")[1]}/{z}/{x}/{y}`],
+      tileSize: 256,
+      minzoom: 8,
+      maxzoom: 16,
+    };
+    return { source };
+  };
+
+  let source;
+
+  onMount(async () => {
+    const generatedSource = await generateCogSource(cogURL);
+    ({ source } = generatedSource);
+  });
+</script>
+
+<Layer type="raster" {source} />
--- sites/SingaporeAndSEABiodiversityHistory/src/components/animal/AnimalMap.svelte
+++ sites/SingaporeAndSEABiodiversityHistory/src/components/animal/AnimalMap.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
-  import { MapLibre } from "svelte-maplibre";
+  import { MapLibre, Layer } from "svelte-maplibre";
   import Animals from "./layers/Animals.svelte";
   import Analyticals from "./layers/Analyticals.svelte";
   import Ports from "./layers/Ports.svelte";
+  import CogLayer from "../COGLayer.svelte";
+  import RasterSource from "../RasterSource.svelte";
 </script>
 
 <MapLibre
@@ -16,7 +18,17 @@
   ]}
   standardControls
 >
+  <RasterSource
+    tiles={[
+      "https://libmaps.nus.edu.sg/gis/rest/services/Sing_Hist_Maps/1975/MapServer/tile/{z}/{y}/{x}",
+    ]}
+  >
+    <Layer type="raster" />
+  </RasterSource>
   <Animals />
   <Analyticals />
   <Ports />
+  <CogLayer
+    cogURL="https://s3.ap-southeast-1.amazonaws.com/static-content-libmaps.nus.edu.sg/1983.tif"
+  />
 </MapLibre>
