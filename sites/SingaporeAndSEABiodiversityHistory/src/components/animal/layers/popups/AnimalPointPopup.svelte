21c0ad5dd5164014efef481f23c1107d37b667e3
tohlh <tohliheng@gmail.com>
2023-07-11 18:28:54 +0800

    Update animal point popup

--- sites/SingaporeAndSEABiodiversityHistory/src/components/animal/layers/popups/AnimalPointPopup.svelte
+++ sites/SingaporeAndSEABiodiversityHistory/src/components/animal/layers/popups/AnimalPointPopup.svelte
@@ -14,20 +14,17 @@
     {#if feature}
       <ul>
         <strong class="underline underline-offset-1">General</strong>
-        <li><strong>Status: </strong>{feature["Status"]}</li>
         <li>
-          <strong>Species Name (Current Accepted): </strong>{feature[
-            "Name_Current"
-          ]}
+          <strong>Species Name (Current Accepted): </strong>
+          <em>{feature["Name_Current"]}</em>
         </li>
         <li><strong>Authority, Year: </strong>{feature["Authority_Year"]}</li>
         <li>
           <strong>Common Name: </strong>{feature["CommonName"]}
         </li>
         <li>
-          <strong>Species Name (Previous Names): </strong>{feature[
-            "PreviousNames"
-          ]}
+          <strong>Species Name (Previous Names): </strong>
+          <em>{feature["PreviousNames"]}</em>
         </li>
         <li><strong>Kingdom: </strong>{feature["Kingdom"]}</li>
         <li><strong>Phylum: </strong>{feature["Phylum"]}</li>
@@ -42,7 +39,8 @@
           ]}
         </li>
         <li>
-          <strong>Native Geographic Range: </strong>{feature["Native_geog"]}
+          <strong>Native Geographic Range: </strong>
+          {feature["Native_geog"]}
         </li>
         <br />
 
@@ -50,123 +48,41 @@
           <strong class="underline underline-offset-1">First Published</strong>
         </li>
         <li>
-          <strong>Species name (First Published): </strong>{feature[
-            "Name_FirstPub"
-          ]}
+          <strong>Species name (First Published): </strong>
+          <em>{feature["Name_FirstPub"]}</em>
         </li>
         <li>
-          <strong>Reference (First Published): </strong>{feature[
-            "Ref_FirstPub"
-          ]}
+          <strong>Reference (First Published): </strong>
+          {feature["Ref_FirstPub"]}
         </li>
         <li>
-          <strong>Specific Page No. (First Published): </strong>{feature[
-            "PageNo_FirstPub"
-          ]}
+          <strong>Specific Page No. (First Published): </strong>
+          {feature["PageNo_FirstPub"]}
         </li>
         <li>
-          <strong>Date: yyyy (First Published): </strong>{feature[
-            "Date_yyyy_FirstPub"
-          ]}
+          <strong>Date: yyyy (First Published): </strong>
+          {feature["Date_yyyy_FirstPub"]}
         </li>
         <li>
-          <strong>Latitude (First Published): </strong>{feature["Lat_FirstPub"]}
+          <strong>Latitude (First Published): </strong>
+          {feature["Lat_FirstPub"]}
         </li>
         <li>
-          <strong>Longitude (First Published): </strong>{feature[
-            "Long_FirstPub"
-          ]}
+          <strong>Longitude (First Published): </strong>
+          {feature["Long_FirstPub"]}
         </li>
         <li>
-          <strong>Location (First published): </strong>{feature[
-            "Location_FirstPub"
-          ]}
+          <strong>Location (First published): </strong>
+          {feature["Location_FirstPub"]}
         </li>
         <br />
 
         <li>
-          <strong class="underline underline-offset-1">First Sighting</strong>
+          <strong class="underline underline-offset-1">
+            Introduction Pathway
+          </strong>
         </li>
-        <li>
-          <strong>Species name (First sighting): </strong>{feature[
-            "Name_FirstSight"
-          ]}
-        </li>
-        <li>
-          <strong>Reference (First sighting): </strong>{feature[
-            "Ref_FirstSight"
-          ]}
-        </li>
-        <li>
-          <strong>Date: dd/mm/yyyy (First sighting): </strong>{feature[
-            "Date_ddmmyyyy_FirstSight"
-          ]}
-        </li>
-        <li>
-          <strong>Year (First sighting) : </strong>{feature["Year_FirstSight"]}
-        </li>
-        <li>
-          <strong>Latitude (First sighting) : </strong>{feature[
-            "Lat_FirstSight"
-          ]}
-        </li>
-        <li>
-          <strong>Longitude (First sighting) : </strong>{feature[
-            "Long_FirstSight"
-          ]}
-        </li>
-        <li>
-          <strong>Location (First sighting): </strong>{feature[
-            "Location_FirstSight"
-          ]}
-        </li>
-        <br />
-
-        <li>
-          <strong class="underline underline-offset-1"
-            >Introduction Pathway</strong
-          >
-        </li>
-        <li>
-          <strong>Introduction Pathway: </strong>{feature["Intro_pathway"]}
-        </li>
-        <li><strong>Introduction Mode: </strong>{feature["Intro_mode"]}</li>
-        <li><strong>Introduction All: </strong>{feature["Intro_all"]}</li>
-        <br />
-
-        <li>
-          <strong class="underline underline-offset-1"> Extinction </strong>
-        </li>
-        <li><strong>Extinction (Y/N): </strong>{feature["Extinction_YN"]}</li>
-        <li>
-          <strong>Year (Extinction): </strong>{feature["Year_Extinction"]}
-        </li>
-        <li>
-          <strong>Extinction Reference: </strong>{feature["Extinction_Ref"]}
-        </li>
-        <br />
-
-        <li>
-          <strong class="underline underline-offset-1"> Active Removal </strong>
-        </li>
-        <li>
-          <strong>Active removal (Y/N): </strong>{feature["ActiveRemoval_YN"]}
-        </li>
-        <li>
-          <strong>Active removal Reference: </strong>{feature[
-            "ActiveRemoval_Ref"
-          ]}
-        </li>
-        <br />
-
-        <li><strong class="underline underline-offset-1">General</strong></li>
-        <li>
-          <strong
-            >References: Subsequent records (Surname, journal short form,
-            vol(issue) (year) page):
-          </strong>{feature["Ref_SubsequentRecords"]}
-        </li>
-        <li><strong>Additional Remarks: </strong>{feature["Add_Remarks"]}</li>
+        <li><strong>Introduction Pathway: </strong>{feature["Intro_all"]}</li>
       </ul>
     {/if}
   </div>
